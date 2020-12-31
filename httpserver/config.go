9803b9b52b9e58a022f1b63c85c38537712cfb1b
Nicolas JUHEL <githubatcom@nabbar.com>
2020-12-31 15:57:38 +0100

    Package httpserver : refactor server, adding pool and add config model (with capbilities of TLS override for some server

--- httpserver/config.go
+++ httpserver/config.go
@@ -1,1 +1,319 @@
 package httpserver
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+
+	libtls "github.com/nabbar/golib/certificates"
+	liberr "github.com/nabbar/golib/errors"
+)
+
+type MapUpdPoolServerConfig func(cfg ServerConfig) ServerConfig
+type MapRunPoolServerConfig func(cfg ServerConfig)
+type PoolServerConfig []ServerConfig
+
+func (p PoolServerConfig) PoolServer() (PoolServer, liberr.Error) {
+	var (
+		r = NewPool()
+		e = ErrorPoolAdd.Error(nil)
+	)
+
+	p.MapRun(func(cfg ServerConfig) {
+		var err liberr.Error
+
+		if r, err = r.Add(cfg.Server()); err != nil {
+			e.AddParentError(err)
+		}
+	})
+
+	if !e.HasParent() {
+		e = nil
+	}
+
+	return r, e
+}
+
+func (p PoolServerConfig) UpdatePoolServer(pSrv PoolServer) (PoolServer, liberr.Error) {
+	var e = ErrorPoolAdd.Error(nil)
+
+	p.MapRun(func(cfg ServerConfig) {
+		var err liberr.Error
+
+		if pSrv, err = pSrv.Add(cfg.Server()); err != nil {
+			e.AddParentError(err)
+		}
+	})
+
+	if !e.HasParent() {
+		e = nil
+	}
+
+	return pSrv, e
+}
+
+func (p PoolServerConfig) Validate() liberr.Error {
+	var e = ErrorPoolValidate.Error(nil)
+
+	p.MapRun(func(cfg ServerConfig) {
+		var err liberr.Error
+
+		if err = cfg.Validate(); err != nil {
+			e.AddParentError(err)
+		}
+	})
+
+	if !e.HasParent() {
+		e = nil
+	}
+
+	return e
+}
+
+func (p PoolServerConfig) MapUpdate(f MapUpdPoolServerConfig) PoolServerConfig {
+	var r = make(PoolServerConfig, 0)
+
+	if p != nil {
+		r = p
+	}
+
+	for i, c := range r {
+		r[i] = f(c)
+	}
+
+	return r
+}
+
+func (p PoolServerConfig) MapRun(f MapRunPoolServerConfig) PoolServerConfig {
+	var r = make(PoolServerConfig, 0)
+
+	if p != nil {
+		r = p
+	}
+
+	for _, c := range r {
+		f(c)
+	}
+
+	return r
+}
+
+type ServerConfig struct {
+	getTLSDefault    func() libtls.TLSConfig
+	getParentContext func() context.Context
+
+	/*** http options ***/
+
+	// ReadTimeout is the maximum duration for reading the entire
+	// request, including the body.
+	//
+	// Because ReadTimeout does not let Handlers make per-request
+	// decisions on each request body's acceptable deadline or
+	// upload rate, most users will prefer to use
+	// ReadHeaderTimeout. It is valid to use them both.
+	ReadTimeout time.Duration `mapstructure:"read_timeout" json:"read_timeout" yaml:"read_timeout" toml:"read_timeout"`
+
+	// ReadHeaderTimeout is the amount of time allowed to read
+	// request headers. The connection's read deadline is reset
+	// after reading the headers and the Handler can decide what
+	// is considered too slow for the body. If ReadHeaderTimeout
+	// is zero, the value of ReadTimeout is used. If both are
+	// zero, there is no timeout.
+	ReadHeaderTimeout time.Duration `mapstructure:"read_header_timeout" json:"read_header_timeout" yaml:"read_header_timeout" toml:"read_header_timeout"`
+
+	// WriteTimeout is the maximum duration before timing out
+	// writes of the response. It is reset whenever a new
+	// request's header is read. Like ReadTimeout, it does not
+	// let Handlers make decisions on a per-request basis.
+	WriteTimeout time.Duration `mapstructure:"write_timeout" json:"write_timeout" yaml:"write_timeout" toml:"write_timeout"`
+
+	// MaxHeaderBytes controls the maximum number of bytes the
+	// server will read parsing the request header's keys and
+	// values, including the request line. It does not limit the
+	// size of the request body.
+	// If zero, DefaultMaxHeaderBytes is used.
+	MaxHeaderBytes int `mapstructure:"max_header_bytes" json:"max_header_bytes" yaml:"max_header_bytes" toml:"max_header_bytes"`
+
+	/*** http2 options ***/
+
+	// MaxHandlers limits the number of http.Handler ServeHTTP goroutines
+	// which may run at a time over all connections.
+	// Negative or zero no limit.
+	MaxHandlers int `mapstructure:"max_handlers" json:"max_handlers" yaml:"max_handlers" toml:"max_handlers"`
+
+	// MaxConcurrentStreams optionally specifies the number of
+	// concurrent streams that each client may have open at a
+	// time. This is unrelated to the number of http.Handler goroutines
+	// which may be active globally, which is MaxHandlers.
+	// If zero, MaxConcurrentStreams defaults to at least 100, per
+	// the HTTP/2 spec's recommendations.
+	MaxConcurrentStreams uint32 `json:"max_concurrent_streams" json:"max_concurrent_streams" yaml:"max_concurrent_streams" toml:"max_concurrent_streams"`
+
+	// MaxReadFrameSize optionally specifies the largest frame
+	// this server is willing to read. A valid value is between
+	// 16k and 16M, inclusive. If zero or otherwise invalid, a
+	// default value is used.
+	MaxReadFrameSize uint32 `json:"max_read_frame_size" json:"max_read_frame_size" yaml:"max_read_frame_size" toml:"max_read_frame_size"`
+
+	// PermitProhibitedCipherSuites, if true, permits the use of
+	// cipher suites prohibited by the HTTP/2 spec.
+	PermitProhibitedCipherSuites bool `json:"permit_prohibited_cipher_suites" json:"permit_prohibited_cipher_suites" yaml:"permit_prohibited_cipher_suites" toml:"permit_prohibited_cipher_suites"`
+
+	// IdleTimeout specifies how long until idle clients should be
+	// closed with a GOAWAY frame. PING frames are not considered
+	// activity for the purposes of IdleTimeout.
+	IdleTimeout time.Duration `json:"idle_timeout" json:"idle_timeout" yaml:"idle_timeout" toml:"idle_timeout"`
+
+	// MaxUploadBufferPerConnection is the size of the initial flow
+	// control window for each connections. The HTTP/2 spec does not
+	// allow this to be smaller than 65535 or larger than 2^32-1.
+	// If the value is outside this range, a default value will be
+	// used instead.
+	MaxUploadBufferPerConnection int32 `json:"max_upload_buffer_per_connection" json:"max_upload_buffer_per_connection" yaml:"max_upload_buffer_per_connection" toml:"max_upload_buffer_per_connection"`
+
+	// MaxUploadBufferPerStream is the size of the initial flow control
+	// window for each stream. The HTTP/2 spec does not allow this to
+	// be larger than 2^32-1. If the value is zero or larger than the
+	// maximum, a default value will be used instead.
+	MaxUploadBufferPerStream int32 `json:"max_upload_buffer_per_stream" json:"max_upload_buffer_per_stream" yaml:"max_upload_buffer_per_stream" toml:"max_upload_buffer_per_stream"`
+
+	// Name is the name of the current server
+	// the configuration allow multipke server, which each one must be identify by a name
+	// If not defined, will use the listen address
+	Name string `mapstructure:"name" json:"name" yaml:"name" toml:"name" validate:"required"`
+
+	// Listen is the local address (ip, hostname, unix socket, ...) with a port
+	// The server will bind with this address only and listen for the port defined
+	Listen string `mapstructure:"listen" json:"listen" yaml:"listen" toml:"listen" validate:"required,hostname_port"`
+
+	// Expose is the address use to call this server. This can be allow to use a single fqdn to multiple server"
+	Expose string `mapstructure:"expose" json:"expose" yaml:"expose" toml:"expose" validate:"required,url"`
+
+	// TLS is the tls configuration for this server.
+	// To allow tls on this server, at least the TLS Config option InheritDefault must be at true and the default TLS config must be set.
+	// If you don't want any tls config, just omit or set an empty struct.
+	TLS libtls.Config `mapstructure:"tls" json:"tls" yaml:"tls" toml:"tls"`
+}
+
+func (c *ServerConfig) SetDefaultTLS(f func() libtls.TLSConfig) {
+	c.getTLSDefault = f
+}
+
+func (c *ServerConfig) SetParentContext(f func() context.Context) {
+	c.getParentContext = f
+}
+
+func (c ServerConfig) GetTLS() (libtls.TLSConfig, liberr.Error) {
+	var def libtls.TLSConfig
+
+	if c.getTLSDefault != nil {
+		def = c.getTLSDefault()
+	}
+
+	return c.TLS.NewFrom(def)
+}
+
+func (c ServerConfig) IsTLS() bool {
+	if ssl, err := c.GetTLS(); err == nil && ssl != nil && ssl.LenCertificatePair() > 0 {
+		return true
+	}
+
+	return false
+}
+
+func (c ServerConfig) getContext() context.Context {
+	var ctx context.Context
+
+	if c.getParentContext != nil {
+		ctx = c.getParentContext()
+	}
+
+	if ctx == nil {
+		return context.Background()
+	}
+
+	return ctx
+}
+
+func (c ServerConfig) GetListen() *url.URL {
+	var (
+		err error
+		add *url.URL
+	)
+
+	if c.Listen != "" {
+		if add, err = url.Parse(c.Listen); err != nil {
+			if host, prt, err := net.SplitHostPort(c.Listen); err == nil {
+				add = &url.URL{
+					Host: fmt.Sprintf("%s:%s", host, prt),
+				}
+			} else {
+				add = nil
+			}
+		}
+	}
+
+	if add == nil && c.Expose != "" {
+		if add, err = url.Parse(c.Expose); err != nil {
+			add = nil
+		}
+	}
+
+	return add
+}
+
+func (c ServerConfig) GetExpose() *url.URL {
+	var (
+		err error
+		add *url.URL
+	)
+
+	if c.Expose != "" {
+		if add, err = url.Parse(c.Expose); err != nil {
+			add = nil
+		}
+	}
+
+	if add == nil {
+		if add = c.GetListen(); add != nil {
+			if c.IsTLS() {
+				add.Scheme = "https"
+			} else {
+				add.Scheme = "http"
+			}
+		}
+	}
+
+	return add
+}
+
+func (c ServerConfig) Validate() liberr.Error {
+	val := validator.New()
+	err := val.Struct(c)
+
+	if e, ok := err.(*validator.InvalidValidationError); ok {
+		return ErrorServerValidate.ErrorParent(e)
+	}
+
+	out := ErrorServerValidate.Error(nil)
+
+	for _, e := range err.(validator.ValidationErrors) {
+		//nolint goerr113
+		out.AddParent(fmt.Errorf("config field '%s' is not validated by constraint '%s'", e.Field(), e.ActualTag()))
+	}
+
+	if out.HasParent() {
+		return out
+	}
+
+	return nil
+
+}
+
+func (c ServerConfig) Server() Server {
+	return NewServer(&c)
+}
--- httpserver/server.go
+++ httpserver/server.go
@@ -1,1 +1,250 @@
 package httpserver
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	"golang.org/x/net/http2"
+
+	liberr "github.com/nabbar/golib/errors"
+	liblog "github.com/nabbar/golib/logger"
+)
+
+const (
+	timeoutShutdown = 10 * time.Second
+	timeoutRestart  = 30 * time.Second
+)
+
+type server struct {
+	run atomic.Value
+	cfg *ServerConfig
+	srv *http.Server
+}
+
+type Server interface {
+	GetConfig() *ServerConfig
+	SetConfig(cfg *ServerConfig)
+
+	GetName() string
+	GetBindable() string
+	GetExpose() string
+
+	IsRunning() bool
+	IsTLS() bool
+	WaitNotify()
+	Merge(srv Server) bool
+
+	Listen(handler http.Handler) liberr.Error
+	Restart()
+	Shutdown()
+}
+
+func NewServer(cfg *ServerConfig) Server {
+	return &server{
+		cfg: cfg,
+		srv: nil,
+	}
+}
+
+func (s *server) GetConfig() *ServerConfig {
+	return s.cfg
+}
+
+func (s *server) SetConfig(cfg *ServerConfig) {
+	s.cfg = cfg
+}
+
+func (s server) GetName() string {
+	if s.cfg.Name == "" {
+		s.cfg.Name = s.GetBindable()
+	}
+
+	return s.cfg.Name
+}
+
+func (s *server) GetBindable() string {
+	return s.cfg.GetListen().Host
+}
+
+func (s *server) GetExpose() string {
+	return s.cfg.GetExpose().String()
+}
+
+func (s *server) IsRunning() bool {
+	return s.run.Load().(bool)
+}
+
+func (s *server) IsTLS() bool {
+	return s.cfg.IsTLS()
+}
+
+func (s *server) setRunning() {
+	s.run.Store(true)
+}
+
+func (s *server) setNotRunning() {
+	s.run.Store(false)
+}
+
+func (s *server) Listen(handler http.Handler) liberr.Error {
+	ssl, err := s.cfg.GetTLS()
+	if err != nil {
+		return err
+	}
+
+	srv := &http.Server{
+		Addr:     s.GetBindable(),
+		ErrorLog: liblog.GetLogger(liblog.ErrorLevel, log.LstdFlags|log.Lmicroseconds, "[http/http2 server '%s']", s.GetName()),
+	}
+
+	if s.cfg.ReadTimeout > 0 {
+		srv.ReadTimeout = s.cfg.ReadTimeout
+	}
+
+	if s.cfg.ReadHeaderTimeout > 0 {
+		srv.ReadHeaderTimeout = s.cfg.ReadHeaderTimeout
+	}
+
+	if s.cfg.WriteTimeout > 0 {
+		srv.WriteTimeout = s.cfg.WriteTimeout
+	}
+
+	if s.cfg.MaxHeaderBytes > 0 {
+		srv.MaxHeaderBytes = s.cfg.MaxHeaderBytes
+	}
+
+	if s.cfg.IdleTimeout > 0 {
+		srv.IdleTimeout = s.cfg.IdleTimeout
+	}
+
+	if ssl.LenCertificatePair() > 0 {
+		srv.TLSConfig = ssl.TlsConfig("")
+	}
+
+	if handler != nil {
+		srv.Handler = handler
+	} else if s.srv != nil {
+		srv.Handler = s.srv.Handler
+	}
+
+	cfg := &http2.Server{}
+
+	if s.cfg.MaxHandlers > 0 {
+		cfg.MaxHandlers = s.cfg.MaxHandlers
+	}
+
+	if s.cfg.MaxConcurrentStreams > 0 {
+		cfg.MaxConcurrentStreams = s.cfg.MaxConcurrentStreams
+	}
+
+	if s.cfg.PermitProhibitedCipherSuites {
+		cfg.PermitProhibitedCipherSuites = true
+	}
+
+	if s.cfg.IdleTimeout > 0 {
+		cfg.IdleTimeout = s.cfg.IdleTimeout
+	}
+
+	if s.cfg.MaxUploadBufferPerConnection > 0 {
+		cfg.MaxUploadBufferPerConnection = s.cfg.MaxUploadBufferPerConnection
+	}
+
+	if s.cfg.MaxUploadBufferPerStream > 0 {
+		cfg.MaxUploadBufferPerStream = s.cfg.MaxUploadBufferPerStream
+	}
+
+	if e := http2.ConfigureServer(srv, cfg); e != nil {
+		return ErrorHTTP2Configure.ErrorParent(e)
+	}
+
+	if s.IsRunning() {
+		s.Shutdown()
+	}
+
+	s.srv = srv
+
+	go func() {
+		ctx, cnl := context.WithCancel(s.cfg.getContext())
+
+		defer func() {
+			cnl()
+			s.setNotRunning()
+		}()
+
+		s.srv.BaseContext = func(listener net.Listener) context.Context {
+			return ctx
+		}
+
+		var err error
+
+		if ssl.LenCertificatePair() > 0 {
+			liblog.InfoLevel.Logf("TLS Server '%s' is starting with bindable: %s", s.GetName(), s.GetBindable())
+
+			s.setRunning()
+			err = s.srv.ListenAndServeTLS("", "")
+		} else {
+			liblog.InfoLevel.Logf("Server '%s' is starting with bindable: %s", s.GetName(), s.GetBindable())
+
+			s.setRunning()
+			err = s.srv.ListenAndServe()
+		}
+
+		if !errors.Is(err, ctx.Err()) {
+			liblog.ErrorLevel.LogErrorCtxf(liblog.NilLevel, "Listen Server '%s'", err, s.GetName())
+		}
+	}()
+
+	return nil
+
+}
+
+func (s *server) WaitNotify() {
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT)
+	signal.Notify(quit, syscall.SIGTERM)
+	signal.Notify(quit, syscall.SIGQUIT)
+
+	select {
+	case <-quit:
+		s.Shutdown()
+	case <-s.cfg.getContext().Done():
+		s.Shutdown()
+	}
+}
+
+func (s *server) Restart() {
+	_ = s.Listen(nil)
+}
+
+func (s *server) Shutdown() {
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutShutdown)
+	defer func() {
+		cancel()
+		s.setNotRunning()
+	}()
+
+	liblog.InfoLevel.Logf("Shutdown Server '%s'...", s.GetName())
+
+	if err := s.srv.Shutdown(ctx); err != nil {
+		liblog.ErrorLevel.Logf("Shutdown Server '%s' Error: %v", s.GetName(), err)
+	}
+}
+
+func (s *server) Merge(srv Server) bool {
+	if x, ok := srv.(*server); ok {
+		s.srv = x.srv
+		return true
+	}
+
+	return false
+}
