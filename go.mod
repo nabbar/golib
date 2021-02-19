b26aa04b848fae1089d9c7e0779e650fc2995716
Nicolas JUHEL <gitlab@nabbar.com>
2021-02-19 12:05:18 +0100

    Refactor Status PKG

--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magefile/mage v1.11.0 // indirect
 	github.com/matcornic/hermes/v2 v2.1.0
 	github.com/mattn/go-runewidth v0.0.10 // indirect
 	github.com/mitchellh/copystructure v1.1.1 // indirect
@@ -48,18 +49,18 @@ require (
 	github.com/rogpeppe/go-internal v1.7.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.1+incompatible
-	github.com/sirupsen/logrus v1.7.0
+	github.com/sirupsen/logrus v1.8.0
 	github.com/spf13/jwalterweatherman v1.1.0
 	github.com/ugorji/go v1.2.4 // indirect
 	github.com/vanng822/go-premailer v1.9.0 // indirect
 	github.com/vbauerster/mpb/v5 v5.4.0
 	github.com/xanzy/go-gitlab v0.44.0
 	github.com/xhit/go-simple-mail v2.2.2+incompatible
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
-	golang.org/x/oauth2 v0.0.0-20210210192628-66670185b0cd
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/sys v0.0.0-20210218155724-8ebf48af031b // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
---
+++ status/interface.go
@@ -0,0 +1,111 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicolas JUHEL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package status
+
+import (
+	"net/http"
+	"sync/atomic"
+
+	"github.com/nabbar/golib/version"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nabbar/golib/router"
+)
+
+const statusOK = "OK"
+const statusKO = "KO"
+
+type Response struct {
+	InfoResponse
+	StatusResponse
+
+	Components []CptResponse `json:"components"`
+}
+
+func (r Response) IsOk() bool {
+	if len(r.Components) < 1 {
+		return true
+	}
+
+	for _, c := range r.Components {
+		if c.Status != statusOK {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (r Response) IsOkMandatory() bool {
+	if len(r.Components) < 1 {
+		return true
+	}
+
+	for _, c := range r.Components {
+		if !c.Mandatory {
+			continue
+		}
+
+		if c.Status != statusOK {
+			return false
+		}
+	}
+
+	return true
+}
+
+type RouteStatus interface {
+	MiddlewareAdd(mdw ...gin.HandlerFunc)
+	HttpStatusCode(codeOk, codeKO, codeWarning int)
+
+	Get(c *gin.Context)
+	Register(prefix string, register router.RegisterRouter)
+	RegisterGroup(group, prefix string, register router.RegisterRouterInGroup)
+
+	ComponentNew(key string, cpt Component)
+	ComponentDel(key string)
+	ComponentDelAll(containKey string)
+}
+
+func New(Name string, Release string, Hash string, msgOk string, msgKo string, msgWarm string) RouteStatus {
+	return &rtrStatus{
+		m:   make([]gin.HandlerFunc, 0),
+		n:   Name,
+		v:   Release,
+		h:   Hash,
+		mOK: msgOk,
+		cOk: http.StatusOK,
+		mKO: msgKo,
+		cKO: http.StatusServiceUnavailable,
+		mWM: msgWarm,
+		cWM: http.StatusOK,
+		c:   make(map[string]*atomic.Value, 0),
+	}
+}
+
+func NewVersion(version version.Version, msgOk string, msgKO string, msgWarm string) RouteStatus {
+	return New(version.GetPackage(), version.GetRelease(), version.GetBuild(), msgOk, msgKO, msgWarm)
+}
