9084fcf1e77023fdeaad21d24139d0fb54e60e4f
salim BOU ARAM <salimamine.bouaram.ext@orange.com>
2024-04-27 19:22:24 +0200

    refactoring user-policy-group-role List outputs

--- aws/role/interface.go
+++ aws/role/interface.go
@@ -43,7 +43,7 @@ type client struct {
 type PoliciesWalkFunc func(err error, pol sdktps.AttachedPolicy) error
 
 type Role interface {
-	List() ([]sdktps.Role, error)
+	List() (*sdkiam.ListRolesOutput, error)
 	Check(name string) (string, error)
 	Add(name, role string) (string, error)
 	Delete(roleName string) error
