7bdadd504b69d12e064634256a3c0552dc02c388
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-11-26 09:44:36 +0100

    compress DetectOnly
    Signed-off-by: Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>

--- archive/compress/interface.go
+++ archive/compress/interface.go
@@ -39,6 +39,36 @@ func Parse(s string) Algorithm {
 	}
 }
 
+func DetectOnly(r io.Reader) (Algorithm, io.ReadCloser, error) {
+	var (
+		err error
+		alg Algorithm
+		bfr = bufio.NewReader(r)
+		buf []byte
+	)
+
+	if buf, err = bfr.Peek(6); err != nil {
+		return None, io.NopCloser(bfr), err
+	}
+
+	// Vérifier le type de compression
+	switch {
+	case Gzip.DetectHeader(buf): // gzip
+		alg = Gzip
+	case Bzip2.DetectHeader(buf): // bzip2
+		alg = Bzip2
+	case LZ4.DetectHeader(buf): // lz4
+		alg = LZ4
+	case XZ.DetectHeader(buf): // xz
+		alg = XZ
+	default:
+		alg = None
+	}
+
+	return alg, io.NopCloser(bfr), nil
+
+}
+
 func Detect(r io.Reader) (Algorithm, io.ReadCloser, error) {
 	var (
 		err error
